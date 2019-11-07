7792a1699aeea6c41697a44102e1237e2638fe7c
Fernando <fernandoamontes@gmail.com>
2019-11-07 07:27:38 -0500

    Removed no longer needed files

--- GoogleTrends.R
+++
@@ -1,7 +0,0 @@
-library(gtrendsR)
-
-usr <- ".."
-psw <- ".."
-gconnect(usr, psw) 
-lang_trend <- gtrends(c("Canlan Ice Sports"), start_date = "2015-06-01", end_date = "2016-08-01")
-plot(lang_trend)
--- updater.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-caffeinate -i /Users/fernandomontes/Dropbox/Courses/R/StockModel-I/Updater.r -v 1 -s 1 -i 1 -p 1 -t 1 -m 1
